ad1b3392f6d7baa0656e6eda2159b227f7f75a2f
David Nuertey Manor <manornuertey@gmail.com>
2024-08-12 21:07:46 +0000

    now to put things together

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
+const { DateTime } = require('luxon')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const pluginNavigation = require('@11ty/eleventy-navigation')
+
+
 module.exports = function (config) {
+  config.addPlugin(pluginRss)
+  config.addPlugin(pluginNavigation)
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  config.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  config.addPassthroughCopy({
+    "global.out.css": "global.css",
+  });
+
   return {
     passthroughFileCopy: true,
     dir: {
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+layout: layouts/base.njk
+metaTitle: David Manor
+metaDescription: Software engineer in Accra, Ghana.
+title: "David here,"
+description: Software Engineer based in Accra Ghana
+templateEngineOverride: njk,md
+---
